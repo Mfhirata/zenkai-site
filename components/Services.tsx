71204b856b46461df0ac95a48ae62a2f255810f8
mfhirata <mohandas_hirata@hotmail.com>
2026-01-11 20:06:13 +0000

    fix: corrige case do import useInView dentro de Services.tsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { useInView } from '../hooks/useInView.ts';
+import { useInView } from '../hooks/useinview.ts';
 
 export const SpeedometerIcon = () => (
   <svg className="w-10 h-10" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
